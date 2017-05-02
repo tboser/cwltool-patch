aaf586183d30f1c502748af0225d166b96d2e349
tboser <tboser@ucsc.edu>
2017-05-02 13:57:03 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import setuptools.command.egg_info as egg_info_cmd
+from setuptools import setup
+
+SETUP_DIR = os.path.dirname(__file__)
+README = os.path.join(SETUP_DIR, 'README.rst')
+
+try:
+    import gittaggers
+
+    tagger = gittaggers.EggInfoFromGit
+except ImportError:
+    tagger = egg_info_cmd.egg_info
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+setup(name='cwltool',
+      version='1.0',
+      description='Common workflow language reference implementation',
+      long_description=open(README).read(),
+      author='Common workflow language working group',
+      author_email='common-workflow-language@googlegroups.com',
+      url="https://github.com/common-workflow-language/cwltool",
+      download_url="https://github.com/common-workflow-language/cwltool",
+      license='Apache 2.0',
+      packages=["cwltool", 'cwltool.tests'],
+      package_dir={'cwltool.tests': 'tests'},
+      package_data={'cwltool': ['schemas/draft-2/*.yml',
+                                'schemas/draft-3/*.yml',
+                                'schemas/draft-3/*.md',
+                                'schemas/draft-3/salad/schema_salad/metaschema/*.yml',
+                                'schemas/draft-3/salad/schema_salad/metaschema/*.md',
+                                'schemas/v1.0/*.yml',
+                                'schemas/v1.0/*.md',
+                                'schemas/v1.0/salad/schema_salad/metaschema/*.yml',
+                                'schemas/v1.0/salad/schema_salad/metaschema/*.md',
+                                'schemas/v1.1.0-dev1/*.yml',
+                                'schemas/v1.1.0-dev1/*.md',
+                                'schemas/v1.1.0-dev1/salad/schema_salad/metaschema/*.yml',
+                                'schemas/v1.1.0-dev1/salad/schema_salad/metaschema/*.md',
+                                'cwlNodeEngine.js']},
+      include_package_data=True,
+      install_requires=[
+          'setuptools',
+          'requests >= 1.0',
+          'ruamel.yaml >= 0.12.4',
+          'rdflib >= 4.2.2, < 4.3.0',
+          'shellescape >= 3.4.1, < 3.5',
+          'schema-salad >= 2.2.20170216125639, < 3',
+          'typing >= 3.5.2, < 3.6'
+      ],
+      setup_requires=[] + pytest_runner,
+      test_suite='tests',
+      tests_require=['pytest'],
+      entry_points={
+          'console_scripts': ["cwltool=cwltool.main:main"]
+      },
+      zip_safe=True,
+      cmdclass={'egg_info': tagger},
+      )
