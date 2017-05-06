d0cba6bb0b177c19ca0617571607c57aaf2dc031
tboser <tboser@ucsc.edu>
2017-05-05 18:07:38 -0700

    patch script

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os
+
+setup(name='cwltool-ucsc',
+      version='1.0.20170217172322',
+      description='A patch for cwltool',
+      url='https://github.com/tboser/cwltool-ucsc',
+      author='UCSC CGL',
+      author_email='tboser@ucsc.edu',
+      license='MIT',
+      packages=['cwltool-ucsc'],
+      zip_safe=False)
+
+
+SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
+PATCH_CMD = 'bash'+SETUP_DIR+'/patch.sh'
+os.system(PATCH_CMD)
