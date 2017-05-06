a3f0c3eb3dd80b0b96115c69359884970251b97f
tboser <tboser@ucsc.edu>
2017-05-05 18:21:32 -0700

    distutils attempt

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+from distutils.command.install import install as DistutilsInstall
 from setuptools import setup
 import os
 
-print "setup for cwltool-ucsc beginning"
+
+class MyInstall(DistutilsInstall):
+
+    def run(self):
+        print "setup for cwltool-ucsc beginning"
+        DistutilsInstall.run(self)
+        SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
+        PATCH_CMD = 'bash'+SETUP_DIR+'/patch.sh'
+        os.system(PATCH_CMD)
 
 setup(name='cwltool-ucsc',
       version='1.0.20170217172322',
@@ -13,8 +22,5 @@ setup(name='cwltool-ucsc',
       author_email='tboser@ucsc.edu',
       license='MIT',
       packages=['cwltool-ucsc'],
+      cmdclass={'install': MyInstall},
       zip_safe=False)
-
-SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
-PATCH_CMD = 'bash'+SETUP_DIR+'/patch.sh'
-os.system(PATCH_CMD)
